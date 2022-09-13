fd56b405f1d4354b0da9cd574c7c9beac219f778
Jonas Øren <jonas.oren@gmail.com>
2022-09-13 09:31:22 +0200

    update

--- main.py
+++ main.py
@@ -79,6 +79,45 @@ def str_to_int(string: str) -> int:
     """
     return int("".join([s for s in string.split() if s.isdigit()]))
 
+def get_feature_model(soup, data_dict):#TODO: args
+    # Get feature model
+    def check_feature_model(text, fully_charged, charge_plus, charged):
+        fully_charged = fully_charged or 'fully charged' in text
+        charge_plus = charge_plus or 'charge plus' in text
+        charged = charged or 'charged' in text
+        return fully_charged, charge_plus, charged
+
+    fully_charged = False
+    charge_plus = False
+    charged = False
+
+    for result in soup.find_all('div', class_='panel'):
+        match_p = result.find_all('p')
+        if match_p:
+            for m in match_p:
+                intro_text = m.text.lower()
+                fully_charged, charge_plus, charged = check_feature_model(intro_text, fully_charged, charge_plus, charged)
+
+    # Check feature model in description
+    description = soup.find('div', class_='u-position-relative')
+    if description:
+        description = description.text.lower()
+        fully_charged, charge_plus, charged = check_feature_model(description, fully_charged, charge_plus, charged)
+
+    # fully charged takes prescedence
+    if fully_charged:
+        feature_model = 'fully_charged'
+    elif charge_plus:
+        feature_model = 'charge_plus'
+    elif charged:
+        feature_model = 'charged'
+    else:
+        feature_model = 'unknown'
+
+    data_dict['feature_model'] = feature_model
+
+    return data_dict
+
 
 def extract_data_from_page(url):
 
@@ -87,19 +126,13 @@ def extract_data_from_page(url):
     data_dict = get_price(soup, data_dict)
     data_dict = get_data_from_second_line(soup, data_dict)
     data_dict = parse_list_in_car_ad_html(soup, data_dict)
+    data_dict = get_feature_model(soup, data_dict)
 
     return data_dict
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-
-    data = pd.DataFrame(columns = ['price', 'Modellår', 'Kilometer', 'farge', 'effekt', 'batterikapasitet' ])
-
-    # Get search page
-    print('Getting search page')
-    soup = get_page(url='https://www.finn.no/car/used/search.html?model=1.749.2000264&price_from=100000&price_to=290000&sort=PUBLISHED_DESC')
-
+def get_data_from_ads(soup, data):
+    ## Loop through ads and collect data
     # Get section containing ads
     ad_section = soup.find(id="page-results")
 
@@ -114,37 +147,59 @@ if __name__ == '__main__':
         print('extracting data from page: ' + link)
         data_dict = extract_data_from_page(url=link)
 
+        data['url'] = link
+
         # Add data to the end of the data frame
         data.loc[len(data)] = data_dict
 
-    print(data)
+    # TODO: save csv
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    return data
+
+def scrape_finn_for_bmw_i3_ad_data():  # TODO add url option
+
+    data = pd.DataFrame(columns = ['price', 'Modellår', 'Kilometer', 'farge', 'effekt', 'batterikapasitet', 'feature_model', 'url'])
+
+    # Get search page
+    print('Getting search page')
+
+    next_page_url='https://www.finn.no/car/used/search.html?model=1.749.2000264&price_from=100000&price_to=290000&sort=PUBLISHED_DESC'
+    current_page = 1
+    another_page = True
+
+    # Check that there is another page
+    while another_page:
+        print('Following articles on ' + next_page_url)
+        soup = get_page(url=next_page_url)
+        data = get_data_from_ads(soup, data)  # TODO: rename to add page data
+        another_page = False
+
+        pages_buttons = soup.find_all('a', class_='pagination__page button button--pill')
+        if pages_buttons:
+            for button in pages_buttons:
+                page_number = int("".join([i for i in button['aria-label'] if i.isnumeric()]))  # Extract numbers from string, join and coerce to int
+                if (page_number == current_page + 1):
+                    current_page = current_page + 1
+
+                    # Follow link to next page
+                    print('Going to page ' + str(current_page))
+                    next_page_url = 'https://www.finn.no/car/used/search.html?' + button['href']
+                    another_page = True
+                    break # TODO test this
+        else:
+            print('Only one page.')
+
+    filename = 'finn_car_data.csv'
+    print('Saving data to' + filename)
+    data.to_csv(filename)
+    return data
+
+if __name__ == '__main__':
+    scrape_finn_for_bmw_i3_ad_data()
 
 
-def check_feature_model(text, fully_charged, charge_plus, charged):
-    fully_charged = fully_charged or 'fully charged' in text
-    charge_plus = charge_plus or 'charge plus' in text
-    charged = charged or 'charged' in text
-    return fully_charged, charge_plus, charged
-
-fully_charged = False
-charge_plus = False
-charged = False
-for result in soup.find_all('div', class_='panel'):
-    
-    match_p = result.find_all('p')
-    if match_p:
-        for m in match_p:
-            intro_text = m.text.lower()
-            fully_charged, charge_plus, charged = check_feature_model(intro_text, fully_charged, charge_plus, charged)
 
 
-# Check feature model in description
-description = soup.find('div', class_='u-position-relative').text.lower()
-fully_charged, charge_plus, charged = check_feature_model(description, fully_charged, charge_plus, charged)
 
-# fully charged takes prescedence
-# TODO
 
 # TODO: loop through all pages
